6a69062690d126949129646bb5f0a1f93207cd1a
Lawrence Cheung <lcheunglci@gmail.com>
2022-08-14 21:23:37 -0700

    Add optimization by adding completion mode in the request

--- Movies.Client/Services/StreamService.cs
+++ Movies.Client/Services/StreamService.cs
@@ -18,7 +18,8 @@ namespace Movies.Client.Services
 
         public async Task Run()
         {
-            await GetPosterWithStream();
+            // await GetPosterWithStream();
+            await GetPosterWithStreamAndCompletionMode();
 
         }
 
@@ -30,6 +31,31 @@ namespace Movies.Client.Services
             request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
             
             var response = await _httpClient.SendAsync(request);
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                response.EnsureSuccessStatusCode();
+
+                using (var streamReader = new StreamReader(stream))
+                {
+                    using (var jsonTextReader = new JsonTextReader(streamReader))
+                    {
+                        var jsonSerializer = new JsonSerializer();
+                        var poster = jsonSerializer.Deserialize<Poster>(jsonTextReader);
+
+                        // do something with the poster
+                    }
+                }
+            }
+        }
+
+        private async Task GetPosterWithStreamAndCompletionMode()
+        {
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead);
             using (var stream = await response.Content.ReadAsStreamAsync())
             {
                 response.EnsureSuccessStatusCode();
