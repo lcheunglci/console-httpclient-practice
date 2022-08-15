6d433a50a3ac3311f412400638d22ca9f7faf9da
Lawrence Cheung <lcheunglci@gmail.com>
2022-08-15 16:05:21 -0700

    Add custom http message hanlder for throwing unauthroize for unit testing

---
+++ Movies.Client/Return401UnauthorizeResponseHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Movies.Client
+{
+    public class Return401UnauthorizeResponseHandler : HttpMessageHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.Unauthorized);
+            return Task.FromResult(response);
+        }
+    }
+}
