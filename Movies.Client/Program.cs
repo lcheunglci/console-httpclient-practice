000c0434db8d4aec7ce80820b266de6d9547cc9a
Lawrence Cheung <lcheunglci@gmail.com>
2022-08-14 20:54:12 -0700

    Add partial update for resource

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -54,10 +54,10 @@ namespace Movies.Client
             // scoped lifetime
 
             // For the CRUD demos
-            serviceCollection.AddScoped<IIntegrationService, CRUDService>();
+            // serviceCollection.AddScoped<IIntegrationService, CRUDService>();
 
             // For the partial update demos
-            // serviceCollection.AddScoped<IIntegrationService, PartialUpdateService>();
+            serviceCollection.AddScoped<IIntegrationService, PartialUpdateService>();
 
             // For the stream demos
             // serviceCollection.AddScoped<IIntegrationService, StreamService>();
--- Movies.Client/Services/PartialUpdateService.cs
+++ Movies.Client/Services/PartialUpdateService.cs
@@ -1,14 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.JsonPatch;
+using Movies.Client.Models;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Movies.Client.Services
 {
     public class PartialUpdateService : IIntegrationService
-    { 
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public PartialUpdateService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
         {
-        }         
+            await PatchResource();
+
+        }
+
+        public async Task PatchResource()
+        {
+            var patchDoc = new JsonPatchDocument<MovieForUpdate>();
+            patchDoc.Replace(m => m.Title, "Updated title");
+            patchDoc.Remove(m => m.Description);
+
+            var serializedChangeSet = JsonConvert.SerializeObject(patchDoc);
+
+            var request = new HttpRequestMessage(HttpMethod.Patch,
+                "api/movies/api/movies/5b1c2b4d-48c7-402a-80c3-cc796ad49c6b");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Content = new StringContent(serializedChangeSet);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json-patch+json");
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var updatedMovie = JsonConvert.DeserializeObject<Movie>(content);
+
+        }
     }
 }
