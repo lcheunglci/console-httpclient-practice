b2d6e6670446cd4cc975bbb122be3c2aa0a1fb03
Lawrence Cheung <lcheunglci@gmail.com>
2022-08-15 16:28:18 -0700

    add moq to test custom handler

--- Movies.UnitTests/TestableClassWithApiAccessUnitTests.cs
+++ Movies.UnitTests/TestableClassWithApiAccessUnitTests.cs
@@ -1,3 +1,5 @@
+using Moq;
+using Moq.Protected;
 using Movies.Client;
 
 namespace Movies.UnitTests
@@ -17,5 +19,32 @@ namespace Movies.UnitTests
             await Assert.ThrowsAsync<UnauthorizedApiAccessException>
                 (() => testableClass.GetMovie(CancellationToken.None));
         }
+
+        [Fact]
+        public async void GetMovie_On401Response_MustThrowUnauthorizationApiAccessException_WithMoq()
+        {
+            var unauthorizedResponseHttpMessageHandlerMock = new Mock<HttpMessageHandler>();
+            unauthorizedResponseHttpMessageHandlerMock
+                .Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>()
+                    )
+                .ReturnsAsync(new HttpResponseMessage()
+                {
+                    StatusCode = System.Net.HttpStatusCode.Unauthorized
+                });
+            var httpClient = new HttpClient(unauthorizedResponseHttpMessageHandlerMock.Object)
+            {
+                BaseAddress = new Uri("http://localhost:57863")
+            };
+
+            var testableClass = new TestableClassWithApiAccess(httpClient);
+
+            await Assert.ThrowsAsync<UnauthorizedApiAccessException>
+                (() => testableClass.GetMovie(CancellationToken.None));
+        }
+
     }
 }
