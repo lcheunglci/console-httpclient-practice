1f98df8d14c8c999a132b23cbcdec9ecfd6b5290
Lawrence Cheung <lcheunglci@gmail.com>
2022-08-15 09:49:54 -0700

    Add cancellation of resource during stream

--- Movies.Client/Program.cs
+++ Movies.Client/Program.cs
@@ -60,10 +60,10 @@ namespace Movies.Client
             // serviceCollection.AddScoped<IIntegrationService, PartialUpdateService>();
 
             // For the stream demos
-            serviceCollection.AddScoped<IIntegrationService, StreamService>();
+            //serviceCollection.AddScoped<IIntegrationService, StreamService>();
 
             // For the cancellation demos
-            // serviceCollection.AddScoped<IIntegrationService, CancellationService>();
+            serviceCollection.AddScoped<IIntegrationService, CancellationService>();
 
             // For the HttpClientFactory demos
             // serviceCollection.AddScoped<IIntegrationService, HttpClientFactoryInstanceManagementService>();
--- Movies.Client/Services/CancellationService.cs
+++ Movies.Client/Services/CancellationService.cs
@@ -1,14 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Marvin.StreamExtensions;
+using Movies.Client.Models;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace Movies.Client.Services
 {
     public class CancellationService : IIntegrationService
     {
+        private static HttpClient _httpClient = new HttpClient(
+           new HttpClientHandler()
+           {
+               AutomaticDecompression = System.Net.DecompressionMethods.GZip
+           });
+
+        public CancellationService()
+        {
+            _httpClient.BaseAddress = new Uri("http://localhost:57863");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+
         public async Task Run()
         {
+            await GetTrailerAndCancel();
         }
+
+        private async Task GetTrailerAndCancel()
+        {
+            var request = new HttpRequestMessage(
+                HttpMethod.Get,
+                $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/trailers/{Guid.NewGuid()}");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.CancelAfter(1000);
+
+            using (var response = await _httpClient.SendAsync(request,
+                HttpCompletionOption.ResponseHeadersRead,
+                cancellationTokenSource.Token))
+            {
+                using (var stream = await response.Content.ReadAsStreamAsync())
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var poster = stream.ReadAndDeserializeFromJson<Trailer>();
+                }
+            }
+        }
+
     }
 }
