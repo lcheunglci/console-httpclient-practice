88495626fcde1bfe7ddb0133cb5d4567ca312f2e
Lawrence LCI <lcheunglci@gmail.com>
2023-08-31 23:27:00 -0700

    logic to CRUD

--- net6/Movies.Client/Program.cs
+++ net6/Movies.Client/Program.cs
@@ -9,6 +9,8 @@ using IHost host = Host.CreateDefaultBuilder(args)
         // register services for DI
         services.AddLogging(configure => configure.AddDebug().AddConsole());
 
+        services.AddHttpClient();
+
         // For the cancellation samples
         // services.AddScoped<IIntegrationService, CancellationSamples>();
 
--- net6/Movies.Client/Services/CRUDSamples.cs
+++ net6/Movies.Client/Services/CRUDSamples.cs
@@ -4,6 +4,20 @@ public class CRUDSamples : IIntegrationService
 {
     public Task RunAsync()
     {
-        throw new NotImplementedException();
+        return GetResourceAsync();
+    }
+
+    public async Task GetResourceAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient();
+        httpClient.BaseAddress = new Uri("http://localhost:5001");
+        httpClient.Timeout = new TimeSpan(0, 0, 30);
+
+        var response = await httpClient.GetAsync("api/movies");
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+        var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(content,
+        new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
     }
 }
