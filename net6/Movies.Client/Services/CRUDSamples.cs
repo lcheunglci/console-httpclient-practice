e62f6e97b445b804a7492813cbaacc73ec93966b
Lawrence LCI <lcheunglci@gmail.com>
2023-09-03 18:16:19 -0700

    Add a post endpoint to create a resource

--- net6/Movies.Client/Services/CRUDSamples.cs
+++ net6/Movies.Client/Services/CRUDSamples.cs
@@ -69,4 +69,39 @@ public class CRUDSamples : IIntegrationService
 
 
     }
+
+
+    public async Task CreateResourceAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var movieToCreate = new MovieForCreation()
+        {
+            Title = "Reservoir Dogs",
+            Description = "After a simple jewelry heist goes terribly wrong, the " +
+            "surviving criminals begin to suspect that one of them is a police informant.",
+            DirectorId = Guid.Parse("d28888e9-2ba9-437a-a40f-e38cb54f9b35"),
+            ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+            Genre = "Crime, Drama"
+        };
+
+        var serializedMovieToCreate = JsonSerializer.Serialize(movieToCreate,
+            _jsonSerializerOptionsWrapper.Options);
+
+        var request = new HttpRequestMessage(HttpMethod.Post, "api/movies");
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        request.Content = new StringContent(serializedMovieToCreate);
+        request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+        var response = await httpClient.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+
+        var content = await response.Content.ReadAsStringAsync();
+
+        var createdMovie = JsonSerializer.Deserialize<Movie>(
+            content, _jsonSerializerOptionsWrapper.Options);
+
+        //httpClient.PostAsync();
+    }
 }
