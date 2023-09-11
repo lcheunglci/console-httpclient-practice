a20c57b22d7e48a0a093a4137fe6df4acc2ee7eb
Lawrence LCI <lcheunglci@gmail.com>
2023-09-10 22:27:01 -0700

    Add an improvement to the stream

--- net6/Movies.Client/Services/LocalStreamsSamples.cs
+++ net6/Movies.Client/Services/LocalStreamsSamples.cs
@@ -18,19 +18,40 @@ public class LocalStreamsSamples : IIntegrationService
 
     public async Task RunAsync()
     {
-        await GetPostWithStreamAsync();
+        // await GetPosterWithStreamAsync();
+        await GetPosterWithStreamAndCompletionModeAsync();
     }
 
-    private async Task GetPostWithStreamAsync()
+    private async Task GetPosterWithStreamAsync()
     {
         var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
 
         var request = new HttpRequestMessage(
             HttpMethod.Get,
             $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
-        request.Headers.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
         using (var response = await httpClient.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var poster = await JsonSerializer.DeserializeAsync<Poster>(
+                stream,
+                _jsonSerializerOptionsWrapper.Options);
+        }
+    }
+
+    private async Task GetPosterWithStreamAndCompletionModeAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead))
         {
             response.EnsureSuccessStatusCode();
 
