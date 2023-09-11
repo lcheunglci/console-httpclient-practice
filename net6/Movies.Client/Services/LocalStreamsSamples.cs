b0909c636e448a3f8ff8454a080f1fee879f95ad
Lawrence LCI <lcheunglci@gmail.com>
2023-09-10 22:36:38 -0700

    Add some benchmarking to compare the different approaches

--- net6/Movies.Client/Services/LocalStreamsSamples.cs
+++ net6/Movies.Client/Services/LocalStreamsSamples.cs
@@ -1,5 +1,6 @@
 ﻿using Movies.Client.Helpers;
 using Movies.Client.Models;
+using System.Diagnostics;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
@@ -19,7 +20,11 @@ public class LocalStreamsSamples : IIntegrationService
     public async Task RunAsync()
     {
         // await GetPosterWithStreamAsync();
-        await GetPosterWithStreamAndCompletionModeAsync();
+        // await GetPosterWithStreamAndCompletionModeAsync();
+
+        await TestMethodAsync(() => GetPosterWithoutStreamAsync());
+        await TestMethodAsync(() => GetPosterWithStreamAsync());
+        await TestMethodAsync(() => GetPosterWithStreamAndCompletionModeAsync());
     }
 
     private async Task GetPosterWithStreamAsync()
@@ -61,4 +66,46 @@ public class LocalStreamsSamples : IIntegrationService
                 _jsonSerializerOptionsWrapper.Options);
         }
     }
+
+    private async Task GetPosterWithoutStreamAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var request = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/{Guid.NewGuid()}");
+        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        using (var response = await httpClient.SendAsync(request))
+        {
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            var poster = JsonSerializer.Deserialize<Poster>(
+                content,
+                _jsonSerializerOptionsWrapper.Options);
+        }
+    }
+
+    public async Task TestMethodAsync(Func<Task> functionToTest)
+    {
+        // warm up
+        await functionToTest();
+
+        // start stopwatch
+        var stopWatch = Stopwatch.StartNew();
+
+        // run requests
+        for (int i = 0; i < 200; i++)
+        {
+            await functionToTest();
+        }
+
+        // stop stopwatch
+        stopWatch.Stop();
+
+        Console.WriteLine($"Elapsed milliseconds without stream: {stopWatch.ElapsedMilliseconds}, "
+            + $"averaging {stopWatch.ElapsedMilliseconds / 200} milliseconds/request");
+    }
+
 }
