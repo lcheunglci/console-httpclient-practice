06d437b0ace321babfc3289de05a9fa7c25c52da
Lawrence LCI <lcheunglci@gmail.com>
2023-09-15 22:22:34 -0700

    add compression when sending the data

--- net6/Movies.Client/Services/CompressionSamples.cs
+++ net6/Movies.Client/Services/CompressionSamples.cs
@@ -1,5 +1,6 @@
 ﻿using Movies.Client.Helpers;
 using Movies.Client.Models;
+using System.IO.Compression;
 using System.Net.Http.Headers;
 using System.Text.Json;
 
@@ -17,7 +18,8 @@ public class CompressionSamples : IIntegrationService
 
     public async Task RunAsync()
     {
-        await GetPosterWithGZipCompressionAsync();
+        // await GetPosterWithGZipCompressionAsync();
+        await PostAndReadPosterWithWithGZipCompressionAsync();
     }
 
     private async Task GetPosterWithGZipCompressionAsync()
@@ -38,6 +40,69 @@ public class CompressionSamples : IIntegrationService
                 stream,
                 _jsonSerializerOptionsWrapper.Options);
         }
+    }
+
+
+    private async Task PostAndReadPosterWithWithGZipCompressionAsync()
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        // generate a movie poster of 5MB
+        var random = new Random();
+        var generatedBytes = new byte[5242880];
+        random.NextBytes(generatedBytes);
+
+        var posterForCreation = new PosterForCreation()
+        {
+            Name = "A new poster for The Big Lebowski",
+            Bytes = generatedBytes
+        };
+
+        using (var memoryContentStream = new MemoryStream())
+        {
+            await JsonSerializer.SerializeAsync(
+                memoryContentStream,
+                posterForCreation);
+
+            memoryContentStream.Seek(0, SeekOrigin.Begin);
+
+            using (var request = new HttpRequestMessage(
+                HttpMethod.Post,
+                "api/movies/d8663e5e-7494-4f81-8739-6e0de1bea7ee/posters/"))
+            {
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+
+                using (var compressedMemoryContentStream = new MemoryStream())
+                {
+                    using (var gzipStream = new GZipStream(compressedMemoryContentStream, CompressionMode.Compress))
+                    {
+                        memoryContentStream.CopyTo(gzipStream);
+                        gzipStream.Flush();
+                        compressedMemoryContentStream.Position = 0;
+
+                        using (var streamContent = new StreamContent(compressedMemoryContentStream))
+                        {
+                            streamContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+                            streamContent.Headers.ContentEncoding.Add("gzip");
+                            request.Content = streamContent;
+
+                            var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseContentRead);
+                            response.EnsureSuccessStatusCode();
+
+                            var stream = await response.Content.ReadAsStreamAsync();
+                            var poster = JsonSerializer.DeserializeAsync<Poster>(stream,
+                                _jsonSerializerOptionsWrapper.Options);
+
+                            // do something with the newly created poster
+                        }
+                    }
+                }
+            }
+
+        }
+
+
     }
 }
 
