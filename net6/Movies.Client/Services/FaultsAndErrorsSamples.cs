3104c54495efc6934d2b40c8045c7079145efedd
Lawrence LCI <lcheunglci@gmail.com>
2023-09-18 22:43:10 -0700

    Add improvements to resilience and reliability

--- net6/Movies.Client/Services/FaultsAndErrorsSamples.cs
+++ net6/Movies.Client/Services/FaultsAndErrorsSamples.cs
@@ -1,4 +1,5 @@
-﻿using Movies.Client.Helpers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Movies.Client.Helpers;
 using Movies.Client.Models;
 using System.Net;
 using System.Net.Http.Headers;
@@ -58,4 +59,66 @@ public class FaultsAndErrorsSamples : IIntegrationService
                 _jsonSerializerOptionsWrapper.Options);
         }
     }
+
+    private async Task PostMovieAndHandleError(CancellationToken cancellationToken)
+    {
+        var httpClient = _httpClientFactory.CreateClient("MoviesAPIClient");
+
+        var movieForCreation = new MovieForCreation()
+        {
+            Title = "Pulp Fiction",
+            Description = "The movie with Zed.",
+            DirectorId = Guid.Parse("d28888e9-2ba9-437a-a40f-e38cb54f9b35"),
+            ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+            Genre = "Crime, Drama"
+        };
+
+        var serializedMovieForCreation = JsonSerializer.Serialize(movieForCreation, _jsonSerializerOptionsWrapper.Options);
+
+        using (var request = new HttpRequestMessage(HttpMethod.Post, "api/movies"))
+        {
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptEncoding.Add(new StringWithQualityHeaderValue("gzip"));
+            request.Content = new StringContent(serializedMovieForCreation);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            using (var response = await httpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, cancellationToken))
+            {
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    // inspect the status code
+                    if (response.StatusCode == HttpStatusCode.BadRequest)
+                    {
+                        // show this to the user
+                        // Console.WriteLine("The request movie cannot be found.");
+
+                        var errorStream = await response.Content.ReadAsStreamAsync();
+
+
+                        var errorAsProblemDetails = await JsonSerializer.DeserializeAsync<ProblemDetails>(errorStream, _jsonSerializerOptionsWrapper.Options);
+
+
+
+                        // var errors = errorAsProblemDetails?.Errors;
+                        // 
+                        return;
+                    }
+                    else if (response.StatusCode == HttpStatusCode.Unauthorized)
+                    {
+                        // trigger a login flow
+                        return;
+                    }
+
+                    // not using a try catch here has more overhead, this way is safer
+                    response.EnsureSuccessStatusCode();
+                }
+
+                var stream = await response.Content.ReadAsStreamAsync();
+                var movie = await JsonSerializer.DeserializeAsync<Movie>(stream, _jsonSerializerOptionsWrapper.Options);
+            }
+        }
+           
+    }
+
 } 
